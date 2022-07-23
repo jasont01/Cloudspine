49955a083d0e838f7c9bb0ab70408efe10c43cdb
Jason Thompson <jason@jasont.us>
2022-07-23 18:20:26 -0400

    fix cors

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors())
+app.use(cors({ origin: true, credentials: true }))
 
 app.use('/api', require('./api/apps'))
 
