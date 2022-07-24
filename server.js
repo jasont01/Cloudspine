57a60ce23485b6afacc0ace02b94e8adcd70e7d6
Jason Thompson <jason@jasont.us>
2022-07-24 10:31:42 -0400

    sync

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const cors = require('cors')
 
 const app = express()
 
-//process.env.NODE_ENV === 'development' || 'https://web-notes.netlify.app',
 const corsOptions = {
   origin: true,
   credentials: true,
